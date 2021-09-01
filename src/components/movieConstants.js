4aca8d60ce2db0fdab32a5f42dbd65b64d2506c6
adamrajch <arajchwald@gmail.com>
2021-09-01 02:34:25 -0500

    vars

--- src/components/movieConstants.js
+++ src/components/movieConstants.js
@@ -1,33 +1,32 @@
-import constants from "../../constants";
 // process.env.TMDB_API_KEY;
 const requests = [
   {
     title: "Top Rated",
-    url: `/movie/top_rated?api_key=${constants.TMDB_API_KEY}&language=en-US`,
+    url: `/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
   },
   {
     title: "Action",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=28`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=28`,
   },
   {
     title: "Mystery",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=9648`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=9648`,
   },
   {
     title: "Romance",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=10749`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=10749`,
   },
   {
     title: "Documentary",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=99`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=99`,
   },
   {
     title: "Animation",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=16`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=16`,
   },
   {
     title: "Thriller",
-    url: `/discover/movie?api_key=${constants.TMDB_API_KEY}&with_genres=53`,
+    url: `/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=53`,
   },
 ];
 
